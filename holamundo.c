b5f45d813d90a72a22684e5982b0b1a232cba777
Guillermo12 <elguillote_17@hotmail.com>
2015-08-17 17:51:20 -0300

    sixteen

--- holamundo.c
+++ holamundo.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(int argc,char** argv){
-	int a=2;
+	int a=1;
 	printf("La variable a vale: %d\n",a);
-	a= a + 14;
+	a= a + 15;
 	printf("La variable a vale: %d\n",a);
 	return 0;
 }
